ef538854a06829cb00ff0085d3aafb6ad3b65e6a
ayden <ayden@eesaenergy.com>
2025-12-03 14:20:52 +0800

    Initialize Spring Boot project with Maven wrapper and basic structure. Added .gitignore, .gitattributes, and essential application files including main application class, properties, and test class.

---
+++ src/main/java/com/eesa/eesa_electricity/EesaElectricityApplication.java
@@ -0,0 +1,13 @@
+package com.eesa.eesa_electricity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EesaElectricityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EesaElectricityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eesa/eesa_electricity/EesaElectricityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eesa.eesa_electricity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EesaElectricityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
